0aad4820f4388f6fbc031aacd64f501c0879c492
Raphael Pierzina <raphael@hackebrot.de>
2016-10-27 16:27:54 +0100

    Add version identifier of package itself

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
+
+// VERSION is the version number of github-release
+const VERSION = "v0.1.0"
 
 func main() {
 	version := flag.String("version", "1.0.0", "version number for the release")
+
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("github-release - %v\n", VERSION))
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
 	fmt.Printf("version number is %v\n", *version)
